01b11669f03873f1c3b4871ca22f7e1fa67ffa93
viktoryboykova <viktoryboykova@list.ru>
2022-01-13 17:10:43 +0300

    Состояние нити

---
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -0,0 +1,18 @@
+package ru.job4j.concurrent;
+
+public class ThreadState {
+
+    public static void main(String[] args) {
+        Thread first = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+        );
+        first.start();
+        Thread second = new Thread(
+                () -> System.out.println(Thread.currentThread().getName())
+        );
+        second.start();
+        while (first.getState() != Thread.State.TERMINATED || second.getState() != Thread.State.TERMINATED) {
+        }
+        System.out.println("Работа завершена");
+    }
+}
